6c3156d70baaebb4b82d21bae5639aab286fcfb4
Anderson Silva da Conceição <61567832+anderson-conceicao@users.noreply.github.com>
2022-04-04 16:01:02 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/devtech/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devtech.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
