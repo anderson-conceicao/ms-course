df0d76e0b032fe7e6d81493bbdd4772d9e0a63da
Anderson Silva da Conceição <61567832+anderson-conceicao@users.noreply.github.com>
2022-04-05 22:25:46 -0300

    Project hr-oauth created.

---
+++ hr-oauth/src/main/java/br/com/devtech/hroauth/HrOauthApplication.java
@@ -0,0 +1,16 @@
+package br.com.devtech.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
